15d780ab1300f4e46fdfe91a59bedbc9eeeb8ac2
Abid Omar <omar.abid2006@gmail.com>
2013-04-29 11:35:30 +0100

    Added admin forms class

--- admin/forms.php
+++ admin/forms.php
@@ -1,8 +1,19 @@
 <?php
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
+
 /**
- * Created by JetBrains PhpStorm.
- * User: root
- * Date: 4/28/13
- * Time: 4:36 PM
- * To change this template use File | Settings | File Templates.
+ * @file
+ *
+ * This class draws the form fields HTML Code. It's a collection of static
+ * functions that can be used without initiation. They were wrapped in a class
+ * for encapsulation.
  */
+if (!class_exists('wp_admin_forms')) {
+    class wp_admin_forms
+    {
+
+    }
+}
