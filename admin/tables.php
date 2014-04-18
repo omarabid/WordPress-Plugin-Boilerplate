59d93e9db7e1097d342c1dfc7e64265e3455a5bd
Abid Omar <contact@omarabid.com>
2014-04-18 13:25:41 +0100

    Table Class

---
+++ admin/tables.php
@@ -0,0 +1,17 @@
+<?php
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+   exit;
+}
+
+/**
+ * @file
+ *
+ * Tables Generator 
+ */
+if (!class_exists('wp_admin_table')) {
+   class wp_admin_table   { 
+
+   }
+}
+	
