8161a4c284b8d40df9f33481d87dd962fb741e5f
Abid Omar <contact@omarabid.com>
2014-10-23 15:01:51 +0100

    More sophisticated ListTable features

--- admin/pages/page2.php
+++ admin/pages/page2.php
@@ -1,51 +1,16 @@
 <?php
-if( ! class_exists( 'WP_List_Table' ) ) {
-   require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
-}
-
-class My_List_Table extends WP_List_Table {
-   var $example_data = array(
-	  array('ID' => 1,'booktitle' => 'Quarter Share', 'author' => 'Nathan Lowell',
-	  'isbn' => '978-0982514542'),
-	  array('ID' => 2, 'booktitle' => '7th Son: Descent','author' => 'J. C. Hutchins',
-	  'isbn' => '0312384378'),
-	  array('ID' => 3, 'booktitle' => 'Shadowmagic', 'author' => 'John Lenahan',
-	  'isbn' => '978-1905548927'),
-	  array('ID' => 4, 'booktitle' => 'The Crown Conspiracy', 'author' => 'Michael J. Sullivan',
-	  'isbn' => '978-0979621130'),
-	  array('ID' => 5, 'booktitle'     => 'Max Quick: The Pocket and the Pendant', 'author'    => 'Mark Jeffrey',
-	  'isbn' => '978-0061988929'),
-	  array('ID' => 6, 'booktitle' => 'Jack Wakes Up: A Novel', 'author' => 'Seth Harwood',
-	  'isbn' => '978-0307454355')
-   );
-   function get_columns(){
-	  $columns = array(
-		 'booktitle' => 'Title',
-		 'author'    => 'Author',
-		 'isbn'      => 'ISBN'
-	  );
-	  return $columns;
-   }
-
-   function prepare_items() {
-	  $columns = $this->get_columns();
-	  $hidden = array();
-	  $sortable = array();
-	  $this->_column_headers = array($columns, $hidden, $sortable);
-	  $this->items = $this->example_data;;
-   }
-   function column_default( $item, $column_name ) {
-	  switch( $column_name ) { 
-	  case 'booktitle':
-	  case 'author':
-	  case 'isbn':
-		 return $item[ $column_name ];
-	  default:
-		 return print_r( $item, true ) ; //Show the whole array for troubleshooting purposes
-	  }
-   }
-}
-
-$myListTable = new My_List_Table();
+$myListTable = new wp_admin_table();
 $myListTable->prepare_items(); 
-$myListTable->display(); 
+?>
+<div class="wrap">
+		<h2><?php _e( 'WP Admin Table', 'wp-pb' ); ?></h2>
+		<form id="edd-payments-filter" method="get" action="<?php echo admin_url( 'admin.php?page=page-id-2' ); ?>">
+			<input type="hidden" name="page" value="" />
+
+			<?php $myListTable->views() ?>
+
+			<?php $myListTable->advanced_filters(); ?>
+
+			<?php $myListTable->display() ?>
+		</form>
+</div>
