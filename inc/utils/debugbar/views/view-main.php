b575531726ceccfc12454398cecb3030c924ad97
Abid Omar <contact@omarabid.com>
2015-03-12 01:52:26 +0100

    renamed POT file

--- inc/utils/debugbar/views/view-main.php
+++ inc/utils/debugbar/views/view-main.php
@@ -1,4 +1,4 @@
-<h3><?php _e( 'Registered Constants', 'bpdn' ); ?></h3>
+<h3><?php _e( 'Registered Constants', 'wpbp' ); ?></h3>
 <ul>
 <?php foreach( $constants as $constant ) { ?>
 <li><strong><?php echo $constant ?></strong>: <em><?php echo constant( $constant ); ?></em></li>
