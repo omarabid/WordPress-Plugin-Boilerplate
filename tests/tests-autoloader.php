6f5d045729079f9af4dd81d3598629e01b178c74
Abid Omar <contact@omarabid.com>
2015-03-14 11:33:10 +0100

    updated tests

--- tests/tests-autoloader.php
+++ tests/tests-autoloader.php
@@ -11,22 +11,22 @@ class Tests_Autoloader extends WP_UnitTestCase {
 		$this->assertEquals( $expected_path, $path );
 
 		$class_name = 'BP_Ajax';
-		$expected_path = 'class-ajax.php';
+		$expected_path = array( 'class-ajax.php', 'ajax/class-ajax.php' );
 		$path = $autoloader->get_file_path_from_class( $class_name );
 		$this->assertEquals( $expected_path, $path );
 
 		$class_name = 'BP_Utils_Logging';
-		$expected_path = 'utils/class-logging.php';
+		$expected_path = array( 'utils/class-logging.php', 'utils/logging/class-logging.php' );
 		$path = $autoloader->get_file_path_from_class( $class_name );
 		$this->assertEquals( $expected_path, $path );
 
 		$class_name = 'BP_Gateways_PayPal_Standard';
-		$expected_path = 'gateways/paypal/class-standard.php';
+		$expected_path = array( 'gateways/paypal/class-standard.php', 'gateways/paypal/standard/class-standard.php' );
 		$path = $autoloader->get_file_path_from_class( $class_name );
 		$this->assertEquals( $expected_path, $path );
 
 		$class_name = 'WCX_Payments_Merchant_Gateways_PayPalStandard';
-		$expected_path = 'payments/merchant/gateways/class-paypalstandard.php';
+		$expected_path = array( 'payments/merchant/gateways/class-paypalstandard.php', 'payments/merchant/gateways/paypalstandard/class-paypalstandard.php' );
 		$path = $autoloader->get_file_path_from_class( $class_name );
 		$this->assertEquals( $expected_path, $path );
 	}
