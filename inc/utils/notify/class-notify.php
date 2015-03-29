5033f050f68b6ca37fb47840a09984888366d7df
Abid Omar <contact@omarabid.com>
2015-03-29 16:27:34 +0100

    docs(notifications): Added documentation to the notifications main class.

--- inc/utils/notify/class-notify.php
+++ inc/utils/notify/class-notify.php
@@ -19,20 +19,52 @@ if ( class_exists( 'BP_Utils_Notify' ) ) {
  */
 class BP_Utils_Notify {
 
+	/**
+	 * Prefix (namespace) of the notices.
+	 *
+	 * @var string
+	 */
 	private $prefix = '';
 
+	/**
+	 * An array of all notices.
+	 *
+	 * @var array
+	 */
 	private $notices = array();
 
+	/**
+	 * An array of active notices.
+	 *
+	 * @var array
+	 */
 	private $active = array();
 
+	/**
+	 * Creates a new instance of the Notifications class.
+	 *
+	 * You should specify the namespace (prefix) of your notices. All your notices will be saved
+	 * and loaded with that prefix.
+	 *
+	 * @param string $prefix
+	 * @return void
+	 */
 	public function __construct( $prefix = 'std' ) {
+		// Set the prefix of the notices
 		$this->prefix = $prefix;
 
+		// Load the notices into the Class
 		$this->load_notices();
 
+		// Display the notices
 		add_action( 'admin_notices', array( &$this, 'display_notices') );
 	}
 
+	/**
+	 * Load all notices into the class.
+	 *
+	 * @return void
+	 */
 	private function load_notices() {
 		$notices = get_option( 'wp_easy_notify_' . $this->prefix, array() );
 		$this->notices = $notices;
@@ -41,9 +73,14 @@ class BP_Utils_Notify {
 		$this->active = $active;
 	}
 
+	/**
+	 * Display the notices.
+	 *
+	 * @return void
+	 */
 	public function display_notices() {
 		foreach( $this->active as $id=>$count ) {
-			$this->display_notice( $this->notices[$id] );
+			$this->print_notification( $this->notices[$id] );
 			if ( $count === 1 ) {
 				unset( $this->active[$id] );
 			}
@@ -55,17 +92,38 @@ class BP_Utils_Notify {
 		$this->save();
 	}
 
-	private function display_notice( $notice ) {
+	/**
+	 * Print a notification to the current Page.
+	 *
+	 * @param array $notice
+	 * @return void
+	 */
+	private function print_notification( $notice ) {
 		echo '<div class="updated '.$notice['type'].'">
-      		'.$notice['content'].' 
-    </div>';
+			'.$notice['content'].' 
+			</div>';
 	}
 
+	/**
+	 * Save the class data to Database.
+	 *
+	 * @return void
+	 */
 	private function save() {
 		update_option( 'wp_easy_notify_' . $this->prefix, $this->notices );
 		update_option( 'wp_easy_notify_active_' . $this->prefix, $this->active );
 	}
 
+	/**
+	 * Add a notification to the notices namespace.
+	 *
+	 * @param string $id
+	 * @param string $content
+	 * @param string $type
+	 * @param bool $hide_button
+	 *
+	 * @return void|bool
+	 */
 	public function add_notification( $id, $content, $type = null, $hide_button = null ) {
 		if ( isset( $this->notices[$id] ) ) {
 			return false;
@@ -89,6 +147,16 @@ class BP_Utils_Notify {
 		$this->save();
 	}
 
+	/**
+	 * Update a notification configuration.
+	 *
+	 * @param string $id
+	 * @param string $content
+	 * @param string $type
+	 * @param bool $hide_button
+	 *
+	 * @return void
+	 */
 	public function update_notification( $id, $content = null, $type = null, $hide_button = null ) {
 		if ( ! isset( $this->notices[$id] ) ) {
 			$this->add_notification( $id, $content, $type, $hide_button );	
@@ -109,30 +177,62 @@ class BP_Utils_Notify {
 		$this->save();
 	}
 
+	/**
+	 * Set notification display to true.
+	 *
+	 * @param string $id
+	 * @param int $life_count
+	 *
+	 * @return void
+	 */
 	public function display_notification( $id, $life_count ) {
 		$this->active[$id] = $life_count;	
 
 		$this->save();
 	}
 
+	/**
+	 * Set notification display to false.
+	 *
+	 * @param string $id
+	 *
+	 * @return void
+	 */
 	public function hide_notification( $id ) {
 		unset( $this->active[$id] );
 
 		$this->save();
 	}
 
+	/**
+	 * Remove a notification from the namespace/database.
+	 *
+	 * @param string $id
+	 *
+	 * @return void
+	 */
 	public function delete_notification( $id ) {
 		unset( $this->notices[$id] );
 
 		$this->save();
 	}
 
+	/**
+	 * Set all notifications display to false.
+	 *
+	 * @return void
+	 */
 	public function hide_all_notifications() {
 		$this->active = array();
 
 		$this->save();
 	}
 
+	/**
+	 * Remove all the notifications from the namespace/database.
+	 *
+	 * @return void
+	 */
 	public function delete_all_notifications() {
 		$this->notices = array();
 
