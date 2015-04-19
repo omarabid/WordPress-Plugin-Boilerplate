5b0ea36560e6df7a25eacbebdb16f4cb62b99c6c
Abid Omar <contact@omarabid.com>
2015-04-19 14:46:23 +0100

    feat(MVC): skeleton classes for Admin MVC

---
+++ app/core/admin_model.php
@@ -0,0 +1,20 @@
+<?php
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+// Exit if class is already defined
+if ( class_exists( 'BP_Admin_Model' ) ) {
+	return;
+}
+
+/**
+ * Admin Model 
+ *
+ * @class BP_Admin_Model 
+ * @package app/core
+ * @author Abid Omar
+ */
+abstract class BP_Admin_Model {
+
+}
