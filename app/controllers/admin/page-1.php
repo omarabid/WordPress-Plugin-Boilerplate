47976e36f7b22e8bd83d21fae8b3e9e25ca8922c
Abid Omar <contact@omarabid.com>
2015-04-19 16:51:33 +0100

    fix(MVC): fixed the controllers names

--- app/controllers/admin/page-1.php
+++ app/controllers/admin/page-1.php
@@ -4,18 +4,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 // Exit if class is already defined
-if ( class_exists( 'BP_Admin_Page_1' ) ) {
+if ( class_exists( 'BP_Admin_Controller_Page_1' ) ) {
 	return;
 }
 
 /**
  * Admin Page 1 
  *
- * @class BP_Admin_Page_1 
+ * @class BP_Admin_Controller_Page_1 
  * @package app/controllers/admin
  * @author Abid Omar
  */
-class BP_Admin_Page_1 extends BP_MVC_Admin_Controller {
+class BP_Admin_Controller_Page_1 extends BP_MVC_Admin_Controller {
 	protected $page_id = 'page-1';
 	protected $child = false;
 	protected $cap = 'manage_options';	
--- app/controllers/admin/page-3.php
+++ app/controllers/admin/page-3.php
@@ -4,18 +4,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 // Exit if class is already defined
-if ( class_exists( 'BP_Admin_Page_3' ) ) {
+if ( class_exists( 'BP_Admin_Controller_Page_3' ) ) {
 	return;
 }
 
 /**
  * Admin Page 3 
  *
- * @class BP_Admin_Page_3
+ * @class BP_Admin_Controller_Page_3
  * @package app/controllers/admin
  * @author Abid Omar
  */
-class BP_Admin_Page_3 extends BP_MVC_Admin_Controller {
+class BP_Admin_Controller_Page_3 extends BP_MVC_Admin_Controller {
 	protected $page_id = 'page-3';
 	protected $child = false;
 	protected $cap = 'manage_options';	
