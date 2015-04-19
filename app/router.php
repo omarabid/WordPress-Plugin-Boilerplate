456528b4d32ed8e2f48a98ed90f2ddb0d50c3cab
Abid Omar <contact@omarabid.com>
2015-04-19 16:52:18 +0100

    fix(MVC): Updated the MVC router

--- app/router.php
+++ app/router.php
@@ -1,9 +1,9 @@
 <?php
 return array(
 	'admin' => array(
-		'page-1' => 'BP_Admin_Page_1',
-		'page-2' => 'BP_Admin_Page_2',
-		'page-3' => 'BP_Admin_Page_3',
+		'page-1' => 'BP_Admin_Controller_Page_1',
+		'page-2' => 'BP_Admin_Controller_Page_2',
+		'page-3' => 'BP_Admin_Controller_Page_3',
 	),
 	'front' => array(
 
