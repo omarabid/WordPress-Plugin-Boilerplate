91d744154fbc8fb0f9228f1833b05e33a03bf836
Abid Omar <contact@omarabid.com>
2015-04-19 17:10:34 +0100

    feat(MVC): Add menu function

--- inc/mvc/class-mvc.php
+++ inc/mvc/class-mvc.php
@@ -17,10 +17,28 @@ if ( class_exists( 'BP_MVC' ) ) {
  */
 class BP_MVC {
 
+	private $app_path;
 	private $admin_pages = array();
 	private $front_pages = array();
 
 	public function __construct( $app_path ) {
+		$this->app_path = $app_path;
+		$this->router = require_once( $app_path . '/router.php' );
+
+		// Admin Pages
+		$this->load_admin_pages();
+		// Front Pages	
+		$this->load_front_pages();
+	}
+
+	private function load_admin_pages() {
+		foreach( $this->router['admin'] as $file=>$class ) {	
+			require_once( $this->app_path . '/controllers/admin/' . $file . '.php' );
+			new $class();	
+		}	
+	}
+
+	private function load_front_pages() {
 
 	}
 }
