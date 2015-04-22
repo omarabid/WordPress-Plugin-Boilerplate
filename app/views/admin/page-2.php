799b5650b662b270ab06e2dc13c6da0863c6b87f
Abid Omar <contact@omarabid.com>
2015-04-22 10:23:42 +0100

    feat(views): Admin Pages Views skeletons

---
+++ app/views/admin/page-2.php
@@ -0,0 +1,20 @@
+<?php
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+// Exit if class is already defined
+if ( class_exists( 'BP_Admin_View_Page_2' ) ) {
+	return;
+}
+
+/**
+ * Admin View Page 2 
+ *
+ * @class BP_Admin_View_Page_2 
+ * @package app/views
+ * @author Abid Omar
+ */
+class BP_Admin_View_Page_2 {
+
+}
---
+++ app/views/admin/page-3.php
@@ -0,0 +1,20 @@
+<?php
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+// Exit if class is already defined
+if ( class_exists( 'BP_Admin_View_Page_3' ) ) {
+	return;
+}
+
+/**
+ * Admin View Page 3 
+ *
+ * @class BP_Admin_View_Page_3 
+ * @package app/views
+ * @author Abid Omar
+ */
+class BP_Admin_View_Page_3 {
+
+}
