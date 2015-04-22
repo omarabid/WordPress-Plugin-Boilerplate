5070a0bee369c6ad0da0b993309c7b2385fc12ff
Abid Omar <contact@omarabid.com>
2015-04-22 20:40:46 +0100

    refactor(admin): removed old admin backend

--- admin/pages/page1.php
+++
@@ -1,3 +0,0 @@
-<?php
-$form = wp_admin_forms::generate_form( 'pb_settings', array( 'pb_general_section' ) );
-echo $form;
--- admin/pages/page3.php
+++
@@ -1,4 +0,0 @@
-<h2>Options Page</h2>
-<?php
-
-	WPBP()->options->build_panel();
