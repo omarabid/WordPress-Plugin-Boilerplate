c15c98280ae5a376974cd340691d9aafa9cc5483
Abid Omar <contact@omarabid.com>
2015-04-22 20:19:27 +0100

    feat(tables): Table Template and Model

--- app/models/admin/page-3.php
+++ app/models/admin/page-3.php
@@ -16,5 +16,24 @@ if ( class_exists( 'BP_Admin_Model_Page_3' ) ) {
  * @author Abid Omar
  */
 class BP_Admin_Model_Page_3 extends BP_MVC_Admin_Model {
-	
+	protected $data = array(
+		'title' => 'Sample WP Generated Table',
+	);	
+
+	public function __construct() {
+		$this->data['admin_url'] = admin_url( 'admin.php?page=page-id-2' );	
+		$this->data['table'] = $this->generate_table();
+	}
+
+	private function generate_table() {
+		require_once( 'tables/sample_table.php' );
+		$myListTable = new wp_admin_table();
+		$myListTable->prepare_items();
+		ob_start();
+		$myListTable->views();
+		$myListTable->advanced_filters();
+		$myListTable->display();
+		$table = ob_get_clean();
+		return $table;
+	}
 }
