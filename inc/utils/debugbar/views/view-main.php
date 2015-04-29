e0f77155a853e551b6cfa77835f89255621005c1
Abid Omar <contact@omarabid.com>
2015-04-29 20:58:21 +0100

    refactor(includes): Removing the old debugging class

--- inc/utils/debugbar/views/view-main.php
+++
@@ -1,6 +0,0 @@
-<h3><?php _e( 'Registered Constants', 'wpbp' ); ?></h3>
-<ul>
-<?php foreach( $constants as $constant ) { ?>
-<li><strong><?php echo $constant ?></strong>: <em><?php echo constant( $constant ); ?></em></li>
-<?php } ?>
-</ul>
