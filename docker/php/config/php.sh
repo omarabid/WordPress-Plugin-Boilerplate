a79c0390b1f84a24d277f1c87c25eb17f7d58eed
Abid Omar <contact@omarabid.com>
2015-05-22 19:19:50 +0100

    Changed directories structure

--- docker/php/config/php.sh
+++ docker/php/config/php.sh
@@ -3,7 +3,10 @@
 # Clean any files 
 cd /wp
 rm -rf /wp/*
-rm -rf /devl/*
+
+# Create our Directories structure
+mkdir /wp/core
+mkdir /wp/dev
 
 # Setup RSA Keys
 mkdir -p ~/.ssh
@@ -13,57 +16,57 @@ mv /.ssh_tmp/known_hosts ~/.ssh/known_hosts
 rm -r /.ssh_tmp
 
 # Download a new version of WordPress
-wp core download --path=/wp --version=$WP_VERSION --allow-root
+wp core download --path=/wp/core --version=$WP_VERSION --allow-root
 
 # Configure WordPress
-wp core config --dbname=$MYSQL_DATABASE --dbuser=root --dbpass=$MYSQL_ROOT_PASSWORD --dbhost=$DB_PORT_3306_TCP_ADDR --allow-root --extra-php <<PHP
+wp core config  --path=/wp/core --dbname=$MYSQL_DATABASE --dbuser=root --dbpass=$MYSQL_ROOT_PASSWORD --dbhost=$DB_PORT_3306_TCP_ADDR --allow-root --extra-php <<PHP
 define( 'WP_DEBUG', true );
 define( 'SAVE_QUERIES', true );
 define( 'WP_DEBUG_LOG', true );
 PHP
 
 # Install WordPress
-wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_USERNAME --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL --allow-root
+wp core install  --path=/wp/core --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_USERNAME --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL --allow-root
 
 # Enable Smart Permalinks
-wp rewrite structure '/%postname%' --allow-root
+wp rewrite structure '/%postname%' --path=/wp/core --allow-root
 
 # Remove Default WordPress Plugins
-wp plugin delete hello --allow-root
-wp plugin delete akismet --allow-root
+wp plugin delete hello --path=/wp/core --allow-root
+wp plugin delete akismet --path=/wp/core --allow-root
 
 # Install dependency Plugins
-array=(${WP_PLUGINS//:/ })
+array=(${WP_PLUGINS//|/ })
 for i in "${!array[@]}"
 do
-    wp plugin install ${array[i]} --activate --allow-root
+    wp plugin install ${array[i]} --activate --path=/wp/core --allow-root
 done
 
 # Install dependency Themes
-array=(${WP_THEMES//:/ })
+array=(${WP_THEMES//|/ })
 for i in "${!array[@]}"
 do
-    wp theme install ${array[i]} --activate --allow-root
+    wp theme install ${array[i]} --activate --path=/wp/core --allow-root
 done
 
 # Install development Plugins
 array=(${WP_DEV_PLUGINS//|/ })
 for i in "${!array[@]}"
 do
-   git clone ${array[i]} /wp/wp-content/plugins/plugin-$i  
-   wp plugin activate plugin-$i --allow-root
-   mv /wp/wp-content/plugins/plugin-$i /devl/plugin-$i
-   ln -s /devl/plugin-$i /wp/wp-content/plugins/plugin-$i
+   git clone ${array[i]} /wp/core/wp-content/plugins/plugin-$i  
+   wp plugin activate plugin-$i --path=/wp/core --allow-root
+   mv /wp/core/wp-content/plugins/plugin-$i /wp/dev/plugin-$i
+   ln -s /wp/dev/plugin-$i /wp/core/wp-content/plugins/plugin-$i
 done
 
 # Install development Themes
-array=(${WP_DEV_THEMES//:/ })
+array=(${WP_DEV_THEMES//|/ })
 for i in "${!array[@]}"
 do
-	git clone ${array[i]} /wp/wp-content/themes/theme-$i 
-	wp theme install ${array[i]} --allow-root
-	mv /wp/wp-content/themes/theme-$i /devl/theme-$i
-   ln -s /devl/theme-$i /wp/wp-content/themes/theme-$i
+	git clone ${array[i]} /wp/core/wp-content/themes/theme-$i 
+	wp theme install ${array[i]} --path=/wp/core --allow-root
+	mv /wp/core/wp-content/themes/theme-$i /wp/dev/theme-$i
+   ln -s /wp/dev/theme-$i /wp/core/wp-content/themes/theme-$i
 done
 
 php-fpm
